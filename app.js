8e0e462466f013ed2070781af9906c1cdb95a645
AFFAN SIDDIQUI <affansiddiqu804@gmail.com>
2024-12-01 17:01:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// // QUESTION 2
+var even = 2
+var odd = 3
+if(even % 3 === 0){
+    console.log('the number is even.');
+}else {
+    console.log('the number is odd.');
+}
+
+// QUESTION 3
+var age = 10
+if(age >18){
+    console.log('Old Enough')
+}else 
+console.log ('Too Young.')
+
+// QUESTION 4
+let name = prompt("Please enter your name:");
+alert("Welcome \n goodmorning!");
+
+
+
+    //  QUESTION 4/2
+    
+var userName = prompt('Enter username')
+var userPass = prompt('Enter password')
+
+if(userName == 'toss' && userPass == 'everywhere'){
+    alert('Correct password')
+}
+else{
+    alert('Incorrect Password')
+}
